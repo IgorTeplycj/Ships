1e1cfce57046c55ad4181308da3aa0c0afca25b1
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-06-08 22:45:22 +0600

    Добавил папку Model

---
+++ HM2/Model/Fields/Commands/SplitFieldCommand.cs
@@ -0,0 +1,55 @@
+﻿using HM2.GameSolve.Interfaces;
+using HM2.IoCs;
+using HM2.Model.Fields.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM2.Model.Fields.Commands
+{
+    public class SplitFieldCommand<T1, T2> : ICommand
+    {
+        double discrete = 0.0;
+        Square<T1> field;
+        List<Square<T2>> quares;
+        int squareX;
+        int squareY;
+        public SplitFieldCommand(Square<T1> field, List<Square<T2>> quares, int squareX, int squareY)
+        {
+            this.field = field;
+            this.squareX = squareX;
+            this.squareY = squareY;
+            this.quares = quares;
+
+            discrete = IoC<double>.Resolve("discrete");
+        }
+        public void Execute()
+        {
+            List<Square<T2>> squares = new List<Square<T2>>();
+            //вычисляем ширину и высоту квадрата
+            int width = (int)(Math.Abs(field.EndX - field.StartX) / squareX);
+            int height = (int)(Math.Abs(field.EndY - field.StartY) / squareY);
+
+
+            for (int y = 0; y < field.EndY; y += height)
+            {
+                double _y = Convert.ToDouble(y);
+                if (y > 0)
+                {
+                    _y += discrete;
+                }
+
+                squares.Add(new Square<T2>(0, 0 + width, _y, y + height));
+
+                for (double x = width; x < field.EndX; x += width)
+                {
+                    squares.Add(new Square<T2>(x + discrete, x + width, _y, y + height));
+                }
+            }
+
+            quares.AddRange(squares);
+        }
+    }
+}
---
+++ HM2/Model/Fields/Square.cs
@@ -0,0 +1,54 @@
+﻿using HM2.Model.Fields.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM2.Model.Fields
+{
+    /// <summary>
+    /// Универсальный тип для создания полей игры
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Square<T> : ISquare
+    {
+
+        public Square(double startX, double endX, double startY, double endY)
+        {
+            StartX = startX;
+            EndX = endX;
+            StartY = startY;
+            EndY = endY;
+
+            objContainer = new List<T>();
+        }
+        public double StartX { get; private set; }
+        public double EndX { get; private set; }
+        public double StartY { get; private set; }
+        public double EndY { get; private set; }
+
+        public List<T> objContainer { get; }
+
+        public void Add(T item)
+        {
+            objContainer.Add(item);
+        }
+
+        public bool Contains(T item)
+        {
+            return objContainer.Contains(item);
+        }
+
+        public void Delete(T item)
+        {
+            objContainer.Remove(item);
+        }
+
+
+        public override string ToString()
+        {
+            return $"x0 = {StartX}, x1 = {EndX}, y0 = {StartY}, y1 = {EndY}";
+        }
+    }
+}
