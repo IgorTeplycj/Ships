70392dabeb5ea53e644605b834c4c02f17a8d32d
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-07-12 18:38:15 +0600

    Увеличил время ожидания асинхронной команды в тесте

---
+++ HM2/Model/Interprets/Parse/IInterpret.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ships.Model.Interprets.Parse
+{
+    interface IInterpret
+    {
+         void Interpret();
+    }
+}
