906698b992584b5e5573c1a077556f8853a8b409
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-07-12 18:51:41 +0600

    Добавил интерфейс IInterpret

---
+++ HM2/Interpret/Parse/GameObject.cs
@@ -0,0 +1,17 @@
+﻿using Ships.Interpret.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ships.Interpret.Parse
+{
+    public class GameObject : IInterpret
+    {
+        public void Interpret()
+        {
+            
+        }
+    }
+}
